adc93e863d7185ad792596e433082b89c8915269
frugiam <frugiam@oregonstate.edu>
2024-01-11 06:42:25 -0800

    average.py

--- average.py
+++ average.py
@@ -1,9 +1,9 @@
 # Project 2a
-Output:'please enter five numbers'
-'-2.4'
-'5.1'
-'6.0'
-'123.8'
-'-19.0'
-'The average of those numbers is'
-'22.7'
+Please enter five numbers.
+-2.4
+5.1
+6.0
+123.8
+-19.0
+The average of those numbers is:
+22.7
