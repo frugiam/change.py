729d15539180c1cd10322305dc26ab37600bb38f
frugiam <frugiam@oregonstate.edu>
2024-01-11 15:38:03 -0800

    average.py

--- average.py
+++ average.py
@@ -1,4 +1,9 @@
 # Project 2a
 
-my_sum = -19.0 + -2.4 + 5.1 + 6.0 + 123.8  # 113.5
-quotient: float = 113.5 / 5  # 22.7
+a=int(input("-2.4"))
+b=int(input("5.1"))
+c=int(input("6.0"))
+d=int(input("123.8"))
+e=int(input("-19.0"))
+average = a+b+c+d+e/5
+print("Average of these five numbers is ,average")
