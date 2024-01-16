1dd867a641ac1ea8f1d6bc6c2923987db26662fc
frugiam <frugiam@oregonstate.edu>
2024-01-15 21:54:39 -0800

    average.py

--- average.py
+++ average.py
@@ -1,9 +1,7 @@
 # Project 2a
-
-a=int(input("-2.4"))
-b=int(input("5.1"))
-c=int(input("6.0"))
-d=int(input("123.8"))
-e=int(input("-19.0"))
-average = a+b+c+d+e/5
-print("22.7")
+"""Please enter five numbers"""
+var = [-2.4 + 5.1 + 6.0 + 123.8 - 19.0]
+var= 113.5
+var = 113.5 / 5
+var = 22.7
+"The average of those numbers is: 22.7"
