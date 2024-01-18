156fa6debab7c86af8e01e49727e896af5b1fd73
frugiam <frugiam@oregonstate.edu>
2024-01-18 12:59:49 -0800

    temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -2,6 +2,7 @@
 # GitHub username: frugiam
 # Date: 01/18/2024
 # Description: Project 2b Write a program that converts Celsius temperatures to Fahrenheit temperatures.
+print("Please enter a Celsius temperature.")
 celsius = float(input('Enter temperature in Celsius: '))
 fahrenheit = (celsius * 1.8) + 32
 print('%0.1f Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))
