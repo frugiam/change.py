1a649dd82b9087f7e06629ff0d9edca8a6f7bb30
frugiam <frugiam@oregonstate.edu>
2024-01-18 12:31:02 -0800

    temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -2,14 +2,12 @@
 # GitHub username: frugiam
 # Date: 01/18/2024
 # Description: Project 2b Write a program that converts Celsius temperatures to Fahrenheit temperatures.
-unit = input("Is this temperature in Celsius or Fahrenheit (C/F): ")
-temp = float(input("Enter the temperature: "))
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
 
-if unit == "C":
-    temp = round((9 * temp) / 5 + 32, 1)
-    print(f"The temperature in Fahrenheit is: {temp}°F")
-elif unit == "F":
-    temp = round((temp - 32) * 5 / 9, 1)
-    print(f"The temperature in Celsius is: {temp}°C")
-else:
-    print(f"{unit} is an invalid unit of measurement")
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = celsius_to_fahrenheit(celsius)
+
+print("Temperature in Fahrenheit:", fahrenheit)
