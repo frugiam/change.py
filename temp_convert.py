051976c03a0bf4339efcae5b9a8d1cef75a090b9
frugiam <frugiam@oregonstate.edu>
2024-01-18 07:34:25 -0800

    temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -4,4 +4,4 @@
 # Description: Project 2b Write a program that converts Celsius temperatures to Fahrenheit temperatures.
 C = int(input('Enter a Celsius temperature:'))
 F = 9 / 5*C + 32
-print(f'Fahrenheit temperature for the given Celsius temperature (C) is (F)')
+print(f"Fahrenheit temperature for the given Celsius temperature (C) is (F)")
