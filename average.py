e43000633a1de7c45288406518af614ca0423d99
frugiam <frugiam@oregonstate.edu>
2024-01-18 07:19:26 -0800

    average.py

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
 # Author: Michelle Frugia
 # GitHub username: frugiam
-# Date: 01/18/2024 Description:
-# Project 2a Write a program that asks the user for five numbers and then prints out the average of those numbers.
+# Date: 01/18/2024
+# Description: Project 2a Write a program that asks the user for five numbers and then prints out the average of those numbers.
 print("Please enter five numbers.")
 result = 0
 for x in range(5):
