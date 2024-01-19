8d9ff41d7eaa0d31df6e63642aece6aabd29bd3c
frugiam <frugiam@oregonstate.edu>
2024-01-19 14:03:29 -0800

    change.py

--- change.py
+++ change.py
@@ -2,19 +2,13 @@
 # GitHub username: frugiam
 # Date: 01/18/2024
 # Description: Project 2c Write a program that asks the user for a (integer) number of cents.
-print('Please enter an amount in cents less than a dollar.')
-def change(num_cents):
-    quarters = num_cents // 25
-    num_cents %= 25
-    dimes = num_cents // 10
-    num_cents %= 10
-    nickels = num_cents // 5
-    num_cents %= 5
-    pennies = num_cents
-
-    return quarters, dimes, nickels, pennies
-
-num_cents = int(input("Enter the number of cents from 0 to 99: "))
-quarters, dimes, nickels, pennies = change(num_cents)
-print(f"The fewest number of coins to represent {num_cents} cents is: ")
-print(f"{quarters} quarters, {dimes} dimes, {nickels} nickels, and {pennies} pennies.")
+cents = int(input("How many cents do you have? "))
+ct = cents
+quarters = cents // 25
+cents -= (quarters*25)
+dimes = cents // 10
+cents -= (dimes * 10)
+nickels = cents // 5
+cents -= (nickels * 5)
+pennies = cents // 1
+print("With "+str(ct)+" cents you can have "+str(quarters)+" quarters, "+str(dimes)+ " dimes, "+str(nickels)+" nickels, "+str(pennies)+" pennies.")
