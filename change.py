47b52a9cf850b1aa963328e2f45d9b310ae8f496
frugiam <frugiam@oregonstate.edu>
2024-01-19 14:21:48 -0800

    change.py

--- change.py
+++ change.py
@@ -2,16 +2,10 @@
 # GitHub username: frugiam
 # Date: 01/18/2024
 # Description: Project 2c Write a program that asks the user for a (integer) number of cents.
-cents = int(input('Please enter an amount in cents less than a dollar.'))
-ct = cents
+cents = int(input('Please enter an amount in cents less than a dollar: '))
 quarters = cents // 25
-cents -= (quarters * 25)
+cents = cents % 25
 dimes = cents // 10
-cents -= (dimes * 10)
-nickels = cents // 5
-cents -= (nickels * 5)
-pennies = cents // 1
-cents -= (pennies * 1)
-print('Your change will be: ')
-print("With " + str(ct) + " cents you can have " + str(quarters) + " quarters, " + str(dimes) + " dimes, " + str(
-    nickels) + "nickels," + str(pennies) + " pennies.")
+nickels = (cents % 10) // 5
+pennies = (cents % 10) % 5
+print('Your change will be: \nQ:', quarters, '\nD:', dimes, '\nN:', nickels, '\nP:', pennies)
